cd7d9ae6372edfb66da9752874dc814f9afe80b5
Влад <zhitnik-vlad@mail.ru>
2021-12-01 19:07:36 +0300

    Добавьте файлы проекта.

---
+++ tic tac toe/Form1 Control.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace Tic_Tac_Toe//каким магическим образом помесить ещё один кусок класса внутрь Form1.cs я не нашёл
+{
+    partial class Game //control event part
+    {
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChangeField(button1, button1_Click);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ChangeField(button2, button2_Click);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ChangeField(button3, button3_Click);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ChangeField(button4, button4_Click);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ChangeField(button5, button5_Click);
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ChangeField(button6, button6_Click);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ChangeField(button7, button7_Click);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ChangeField(button8, button8_Click);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ChangeField(button9, button9_Click);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            PlayerMove = true;
+            foreach (var item in Field)
+            {
+                item.Enabled = true;
+                item.UseVisualStyleBackColor = true;
+                item.Text = "";
+                item.Click += null;
+            }
+            button1.Click -= new System.EventHandler(this.button1_Click);
+            button1.Click += new System.EventHandler(this.button1_Click);
+            button2.Click -= new System.EventHandler(this.button2_Click);
+            button2.Click += new System.EventHandler(this.button2_Click);
+            button3.Click -= new System.EventHandler(this.button3_Click);
+            button3.Click += new System.EventHandler(this.button3_Click);
+            button4.Click -= new System.EventHandler(this.button4_Click);
+            button4.Click += new System.EventHandler(this.button4_Click);
+            button5.Click -= new System.EventHandler(this.button5_Click);
+            button5.Click += new System.EventHandler(this.button5_Click);
+            button6.Click -= new System.EventHandler(this.button6_Click);
+            button6.Click += new System.EventHandler(this.button6_Click);
+            button7.Click -= new System.EventHandler(this.button7_Click);
+            button7.Click += new System.EventHandler(this.button7_Click);
+            button8.Click -= new System.EventHandler(this.button8_Click);
+            button8.Click += new System.EventHandler(this.button8_Click);
+            button9.Click -= new System.EventHandler(this.button9_Click);
+            button9.Click += new System.EventHandler(this.button9_Click);
+            button11.Click -= new System.EventHandler(this.button11_Click);
+            button11.Click += new System.EventHandler(this.button11_Click);
+            button11.Enabled = true;
+            button12.Click -= new System.EventHandler(this.button12_Click);
+            button12.Click += new System.EventHandler(this.button12_Click);
+            button13.Click -= new System.EventHandler(this.button13_Click);
+            button13.Click += new System.EventHandler(this.button13_Click);
+        }//reset
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            PlayerMove = false;
+            FirstStepPC();
+            button11.Click -= button11_Click;
+            button11.Enabled = false;
+        }//second
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            button13.Enabled = true;
+            button12.Enabled = false;
+            Mode = false;
+        }//medium
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            button13.Enabled = false;
+            button12.Enabled = true;
+            Mode = true;
+        }//hard
+
+    }
+}
---
+++ tic tac toe/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    struct Point
+    {
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public int x;
+        public int y;
+    }
+
+    public partial class Game : Form//game logic part
+    {
+        public Game()
+        {
+            InitializeComponent();
+            Field[0, 0] = button1;
+            Field[0, 1] = button2;
+            Field[0, 2] = button3;
+            Field[1, 0] = button4;
+            Field[1, 1] = button5;
+            Field[1, 2] = button6;
+            Field[2, 0] = button7;
+            Field[2, 1] = button8;
+            Field[2, 2] = button9;
+        }
+
+        private bool Mode = false;
+
+        private bool PlayerMove = true;
+
+        private string PlayerS = "X";
+
+        private string PC = "O";
+
+        private readonly Button[,] Field = new Button[3, 3];
+
+        private readonly List<List<Point>> Points = new List<List<Point>>
+        {
+            new List<Point>
+            {
+                new Point(2, 0), new Point(1, 1), new Point(0, 2)
+            },
+            new List<Point>
+            {
+                new Point(0, 0), new Point(1, 1), new Point(2, 2)
+            },
+            new List<Point>
+            {
+                new Point(0, 0), new Point(0, 1), new Point(0, 2)
+            },
+            new List<Point>
+            {
+                new Point(1, 0), new Point(1, 1), new Point(1, 2)
+            },
+            new List<Point>
+            {
+                new Point(2, 0), new Point(2, 1), new Point(2, 2)
+            },
+            new List<Point>
+            {
+                new Point(0, 0), new Point(1, 0), new Point(2, 0)
+            },
+            new List<Point>
+            {
+                new Point(0, 1), new Point(1, 1), new Point(2, 1)
+            },
+            new List<Point>
+            {
+                new Point(0, 2), new Point(1, 2), new Point(2, 2)
+            }
+        };
+
+        private void Search()
+        {
+            var variable = Finder(Field);
+            int point = new Random().Next(variable.Count);
+            if (variable.Count > 0)
+                Field[variable[point].x, variable[point].y].PerformClick();
+            else
+            {
+                var heap = Algorithm(Field);
+                List<Point> Hard = new List<Point>();
+                List<Point> Medium = new List<Point>();
+                foreach (var item in heap)
+                {
+                    if (item.Value == 'A')
+                        Hard.Add(item.Key);
+                    else
+                        Medium.Add(item.Key);
+                }
+                if (Mode)//Hard
+                {
+                    if (Hard.Count > 0)
+                    {
+                        Point hard = Hard[new Random().Next(Hard.Count)];
+                        Field[hard.x, hard.y].PerformClick();
+                    }
+                }
+                else
+                {
+                    if (Medium.Count > 0)
+                    {
+                        Point medium = Medium[new Random().Next(Medium.Count)];
+                        Field[medium.x, medium.y].PerformClick();
+                    }
+                }
+            }
+        }
+
+        private List<Point> Finder(Button[,] buttons, bool defence = false)
+        {
+            int countA = 0, countB = 0;
+            int freeCount = 0;
+            Point xy = new Point();
+            List<Point> result = new List<Point>();
+            foreach (var line in Points)
+            {
+                foreach (var point in line)
+                    switch (buttons[point.x, point.y].Text)
+                    {
+                        case "X":
+                            countA++;
+                            break;
+                        case "O":
+                            countB++;
+                            break;
+                        case "":
+                            {
+                                freeCount++;
+                                xy.x = point.x;
+                                xy.y = point.y;
+                            }
+                            break;
+                    }
+                if (countB == 2 && freeCount == 1 && !defence) //  O winer
+                {
+                    result.Clear();
+                    result.Add(new Point(xy.x, xy.y));
+                    return result;
+                }
+                if (countA == 2 & freeCount == 1)
+                    result.Add(new Point(xy.x, xy.y));
+                countA = 0;
+                countB = 0;
+                xy = new Point();
+                freeCount = 0;
+            }
+            return result;
+        }
+
+        private bool Final(Button[,] array = null, char mode = 'b') //both , X, Y
+        {
+            array ??= Field;
+            int countA = 0, countB = 0;
+            foreach (var line in Points)
+            {
+                foreach (var item in line)
+                {
+                    switch (array[item.x, item.y].Text)
+                    {
+                        case "X":
+                            countA++;
+                            break;
+                        case "O":
+                            countB++;
+                            break;
+                        default:
+                            break;
+                    }
+                    if (countA == 3 && (mode == 'b' || mode == 'X'))
+                        return true;
+                    if (countB == 3 && (mode == 'b' || mode == 'Y'))
+                        return true;
+                }
+                countA = 0;
+                countB = 0;
+            }
+            return false;
+        }
+
+        private Dictionary<Point, char> Algorithm(Button[,] array, string symbol = "O", int loop = 0)
+        {
+            Dictionary<Point, char> result = new Dictionary<Point, char>();
+            var voidArray = CopyArray(array);
+            Button[,] assumption = new Button[3, 3];
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    assumption[i, j] = new Button
+                    {
+                        Text = array[i, j].Text
+                    };
+            if (symbol == PC)
+            {
+                foreach (var item in Finder(assumption).Count == 0 ? voidArray : Finder(assumption))
+                {
+                    assumption[item.x, item.y].Text = symbol;
+                    char s;
+                    s = Algorithm(assumption, PlayerS, loop + 1)[new Point(9, 9)];
+                    result.Add(item, s);
+                    assumption[item.x, item.y].Text = "";
+                }
+                if (loop == 0)
+                    return result;
+                foreach (var item in result)
+                    if (item.Value == 'A')
+                    {
+                        result.Add(new Point(9, 9), 'A');
+                        return result;
+                    }
+                result.Add(new Point(9, 9), 'B');
+                return result;
+            }
+            else
+            {
+                
+                foreach (var item in Finder(assumption).Count == 0 ? voidArray : Finder(assumption))
+                {
+                    assumption[item.x, item.y].Text = symbol;
+                    var prediction = Finder(assumption, true);
+                    if (prediction.Count > 1)
+                        result.Add(item, 'B'); 
+                    else if (loop < 3)
+                    {
+                        var s = Algorithm(assumption, PC, loop + 1);
+                        result.Add(item, s[new Point(9, 9)]);
+                    }
+                    else
+                        result.Add(item, 'A');
+                    assumption[item.x, item.y].Text = "";
+                }
+                foreach (var item in result)
+                    if (item.Value == 'B')
+                    {
+                        result.Add(new Point(9, 9), 'B');
+                        return result;
+                    }
+                result.Add(new Point(9, 9), 'A');
+                return result;
+            }
+        }
+
+        private static List<Point> CopyArray(Button[,] array)
+        {
+            List<Point> cloneArray = new List<Point>();
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                    if (array[i, j].Text == "")
+                        cloneArray.Add(new Point(i, j));
+            return cloneArray;
+        }
+
+        private void FakeAlgorithm()
+        {
+            List<Button> Fake = new List<Button>();
+            foreach (var item in Field)
+                if (item.Text == "")
+                    Fake.Add(item);
+            int count = Fake.Count;
+            if (count > 0)
+                Fake[new Random().Next(count - 1)].PerformClick();
+        } //самый вспомогательный костыль, всё ещё рабочий
+
+        private void FirstStepPC()
+        {
+            Field[new Random().Next(3), new Random().Next(3)].PerformClick();
+        }
+
+        private void ChangeField(Button button, EventHandler _event)
+        {
+            button11.Click -= button11_Click;
+            button11.Enabled = false;
+            button12.Click -= button12_Click;
+            button13.Click -= button13_Click;
+            if (PlayerMove)
+            {
+                button.Text = PlayerS;
+                button.ForeColor = Color.Red;
+                PlayerMove = false;
+            }
+            else
+            {
+                button.Text = PC;
+                button.ForeColor = Color.Blue;
+                PlayerMove = true;
+            }
+            if (Final(Field))
+            {
+                foreach (var item in Field)
+                    item.Enabled = false;
+            }
+            button.UseVisualStyleBackColor = false;
+            button.Click -= _event;
+            if (!PlayerMove)
+                Search();
+        }
+    }
+}
