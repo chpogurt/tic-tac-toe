1840777e8eb2af0688268d8242f3ddbc40f5f0c2
Влад <zhitnikvlad13@gmail.com>
2021-12-08 12:16:20 +0300

    Добавил описание методов Добавил ключи для вариативного использования метода Finder

--- tic tac toe/Form1.cs
+++ tic tac toe/Form1.cs
@@ -29,7 +29,10 @@ namespace Tic_Tac_Toe
 
         private readonly Button[,] Field = new Button[3, 3];
 
-        private readonly List<List<Point>> Points = new List<List<Point>>
+        /// <summary>
+        /// Вертикали, горизонтали и диагонали
+        /// </summary>
+        private readonly List<List<Point>> linesList = new List<List<Point>>
         {
             new List<Point>
             {
@@ -65,6 +68,9 @@ namespace Tic_Tac_Toe
             }
         };
 
+        /// <summary>
+        /// Поиск ответоного хода компьютером
+        /// </summary>
         private void Search()
         {
             var variable = Finder(Field);
@@ -106,13 +112,19 @@ namespace Tic_Tac_Toe
                 RandomizeEmpty();
         }
 
-        private List<Point> Finder(Button[,] buttons, bool defence = false)
+        /// <summary>
+        /// Проверяет вертикали, горизонтали и диагонали на предмет опасных ситуаций
+        /// </summary>
+        /// <param name="buttons"> Поле для проверки.</param>
+        /// <param name="mode"> null- дефолтный, false- только вилки "O", true- только вилки "X".</param>
+        /// <returns>Возвращает список возможных ходов, в том числе пустой.</returns>
+        private List<Point> Finder(Button[,] buttons, bool? mode = null)
         {
             int countA = 0, countB = 0;
             int freeCount = 0;
             Point xy = new Point();
             List<Point> result = new List<Point>();
-            foreach (var line in Points)
+            foreach (var line in linesList)
             {
                 foreach (var point in line)
                     switch (buttons[point.x, point.y].Text)
@@ -131,13 +143,15 @@ namespace Tic_Tac_Toe
                             }
                             break;
                     }
-                if (countB == 2 && freeCount == 1 && !defence) //  O winer
+                if (countB == 2 && freeCount == 1 && mode == null ) //  O winer
                 {
-                    result.Clear();
-                    result.Add(new Point(xy.x, xy.y));
-                    return result;
+                        result.Clear();
+                        result.Add(new Point(xy.x, xy.y));
+                        return result;
                 }
-                if (countA == 2 & freeCount == 1)
+                if (countB == 2 && freeCount == 1 && mode == false)
+                    result.Add(new Point(xy.x, xy.y));
+                if (countA == 2 & freeCount == 1 && (mode == null || mode == true))
                     result.Add(new Point(xy.x, xy.y));
                 countA = 0;
                 countB = 0;
@@ -147,11 +161,17 @@ namespace Tic_Tac_Toe
             return result;
         }
 
-        private bool Final(Button[,] array = null, char mode = 'b') //both , X, Y
+        /// <summary>
+        /// Проверят игровое поле на предмет завершения игры
+        /// </summary>
+        /// <param name="array">Массив для проверки</param>
+        /// <param name="mode">Ключи X/O ограничивают проверку одной из сторон, b(оба) - дефолт </param>
+        /// <returns>Возвращает true если игра завершена</returns>
+        private bool Final(Button[,] array = null, char mode = 'b') //both , X, O
         {
             array ??= Field;
             int countA = 0, countB = 0;
-            foreach (var line in Points)
+            foreach (var line in linesList)
             {
                 foreach (var item in line)
                 {
@@ -168,7 +188,7 @@ namespace Tic_Tac_Toe
                     }
                     if (countA == 3 && (mode == 'b' || mode == 'X'))
                         return true;
-                    if (countB == 3 && (mode == 'b' || mode == 'Y'))
+                    if (countB == 3 && (mode == 'b' || mode == 'O'))
                         return true;
                 }
                 countA = 0;
@@ -187,6 +207,15 @@ namespace Tic_Tac_Toe
          * этому, но просчитывать нужно с 3го по 5 ходы, то есть 3, а не 4 хода как тут
          * а благоприятнымм считаются варианты при которых существую варианты построить вилку
          */
+
+
+        /// <summary>
+        /// Просчитывает ходы на 4 шага вперёд, рекурсионный перебор
+        /// </summary>
+        /// <param name="array">Массив на просчёт</param>
+        /// <param name="symbol">Не трогать. Аргумент хода(ИИ / Игрок) </param>
+        /// <param name="loop">Не трогать. Текущее погружение рекурсии</param>
+        /// <returns>Возвращает словарь возможных ходов с ключом качества данного хода</returns>
         private Dictionary<Point, char> Algorithm(Button[,] array, string symbol = "O", int loop = 0)
         {
             Dictionary<Point, char> result = new Dictionary<Point, char>();
@@ -221,7 +250,6 @@ namespace Tic_Tac_Toe
             }
             else
             {
-
                 foreach (var item in Finder(assumption).Count == 0 ? voidArray : Finder(assumption))
                 {
                     assumption[item.x, item.y].Text = symbol;
@@ -248,6 +276,12 @@ namespace Tic_Tac_Toe
             }
         }
 
+
+        /// <summary>
+        /// Находит пустые клетки 
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns>Возвращает список допустимых ходов</returns>
         private static List<Point> FindVoidPoint(Button[,] array)
         {
             List<Point> cloneArray = new List<Point>();
@@ -258,22 +292,33 @@ namespace Tic_Tac_Toe
             return cloneArray;
         }
 
+        /// <summary>
+        /// Генератор случайного хода
+        /// </summary>
         private void RandomizeEmpty()
         {
-            List<Button> Fake = new List<Button>();
+            List<Button> any = new List<Button>();
             foreach (var item in Field)
                 if (item.Text == "")
-                    Fake.Add(item);
-            int count = Fake.Count;
+                    any.Add(item);
+            int count = any.Count;
             if (count > 0)
-                Fake[new Random().Next(count - 1)].PerformClick();
+                any[new Random().Next(count - 1)].PerformClick();
         } //самый вспомогательный костыль, всё ещё рабочий
 
+        /// <summary>
+        /// Первый ход за компьютером
+        /// </summary>
         private void FirstStepPC()
         {
             Field[new Random().Next(3), new Random().Next(3)].PerformClick();
         }
 
+        /// <summary>
+        /// Обработка игровой логики
+        /// </summary>
+        /// <param name="button">Нажатая кнопка</param>
+        /// <param name="_event">Событие вызвавшее метод</param>
         private void ChangeField(Button button, EventHandler _event)
         {
             button11.Click -= button11_Click;
@@ -281,25 +326,25 @@ namespace Tic_Tac_Toe
             button12.Click -= button12_Click;
             button13.Click -= button13_Click;
             button14.Click -= button14_Click;
-            if (PlayerMove)
+            if (PlayerMove) //установка флага игрока
             {
                 button.Text = PlayerS;
                 button.ForeColor = Color.Red;
                 PlayerMove = false;
             }
-            else
+            else //установка флага компьютера
             {
                 button.Text = PC;
                 button.ForeColor = Color.Blue;
                 PlayerMove = true;
             }
-            if (Final(Field))
+            if (Final(Field)) //проверка игры на завершение
             {
                 foreach (var item in Field)
                     item.Enabled = false;
             }
-            button.UseVisualStyleBackColor = false;
-            button.Click -= _event;
+            button.UseVisualStyleBackColor = false; 
+            button.Click -= _event; //отписка события от кнопки
             if (!PlayerMove)
                 Search();
         }
